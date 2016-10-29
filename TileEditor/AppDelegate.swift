c31691b12b3b862ce468b28e7486bbcc4136f9bd
iury bessa <ibessa@ymail.com>
2016-10-28 20:03:14 -0400

    Initial Commit

---
+++ TileEditor/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  TileEditor
+//
+//  Created by iury bessa on 10/28/16.
+//  Copyright © 2016 yellokrow. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ TileEditor/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  TileEditor
+//
+//  Created by iury bessa on 10/28/16.
+//  Copyright © 2016 yellokrow. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
